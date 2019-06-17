ab27aeacfd9f501172d9aced2d0968d656f9d5b5
cu3l3bre <cu3l3bre@googlemail.com>
2019-06-17 13:10:53 +0200

    included sql lite exmaple

--- SnakeSFML/main.cpp
+++ SnakeSFML/main.cpp
@@ -1,12 +1,107 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+//																							//
+//	Author:		DS																			//
+//	Created:	2019.06.17																	//
+//																							//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include "sqlite3.h"
 
 using namespace std;
 
 int main()
 {
+
+	// öffnen der Datenabnk
+
+
+	const char* dateiname = "Snake_Highscores.sqlite";
+
+	ifstream dateistrom(dateiname);
+
+	if (!dateistrom.good()) {
+		cout << "Datei kann nicht geöffnet werden" << endl;
+	}
+	else {
+
+		// Objekt für die Datenbank anlegen
+		sqlite3* datenbank;
+
+		try {
+			sqlite3_open(dateiname, &datenbank);
+
+			
+
+			const char* sqlSelectString = "SELECT * FROM Highscores"; 
+
+			char* fehlermeldung;
+
+			// Objekt für das Ergebnis vorbreiten
+			char ** ergebnisse = NULL;
+			int anzahlSpalten;
+			int anzahlZeilen;
+
+			int fehlercode = sqlite3_get_table(datenbank, sqlSelectString, &ergebnisse, &anzahlZeilen, &anzahlSpalten, &fehlermeldung);
+
+			int fehlercode2 = sqlite3_exec(datenbank, "INSERT INTO..", NULL, NULL, &fehlermeldung);
+
+			// wenn ein fhler aufgetreten
+			if (fehlercode != 0) {
+				cout << "Fehler: " << fehlermeldung << endl;
+			}
+			else {
+
+
+				//ergebnisse = [0,0][1,0][1,0][1,1][2,0][2,1]
+
+				// eins weiter gehen, um auch die tabellenköpfe mit anzuzeigen
+				for (int zeile = 0; zeile <= anzahlZeilen; zeile++) {
+
+					for (int spalte = 0; spalte < anzahlSpalten; spalte++) {
+
+						// Position in der Ergebnisluiste berechene
+						int datenposition = (zeile * anzahlSpalten) + spalte;
+
+						cout << ergebnisse[datenposition] << "  ";
+
+					}
+
+					// Zeilenumbruch nach jeder Zeile
+					cout << endl;
+
+				}
+
+
+			}
+
+			//speicher wieder freigeben
+			sqlite3_free_table(ergebnisse);
+
+		}
+		catch (exception ex) {
+			cout << "Verbindung kann nicht geöffnet werden" << endl;
+		}
+
+
+	}
+
+	dateistrom.close();
+
+
+	// anzeige aller Daten in der tablle highscore
+
+
+
+
+
+
+
 	sf::RenderWindow window(sf::VideoMode(500, 500), "Snake Game using SFML!");
 
 
@@ -31,7 +126,7 @@ int main()
 	test.y = 20;
 
 	boundary.setSize(test);
-	boundary.setPosition(200, 30);
+	boundary.setPosition(-10, -10);
 	boundary.setFillColor(sf::Color::Red);
 
 /*
@@ -70,7 +165,7 @@ int main()
 
 
 		sf::sleep(timeTest);
-		cout << "Loop" << endl;
+		//cout << "Loop" << endl;
 
 		sf::Event event;
 		while (window.pollEvent(event))
@@ -121,8 +216,8 @@ int main()
 		newBodyPart.setFillColor(sf::Color::Green);
 		newBodyPart.setPosition(x, y);
 		snakebody.push_back(newBodyPart);
-		x += 20;
-		y += 20;
+		x += 30;
+		y += 30;
 
 
 		window.clear();
